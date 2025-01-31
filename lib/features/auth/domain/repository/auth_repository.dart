52915ec74dd045ecc12165eff7f15e4ad691ba1a
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-01-31 13:42:06 +0200

    Created contract classes (repoitory & data source) for auth feature

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:trait_lens/core/errors/exceptions.dart';
+import 'package:trait_lens/features/auth/domain/entities/user_entity.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<ServerException, UserEntity>> signInWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<Either<ServerException, UserEntity>> signUpWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<Either<ServerException, UserEntity>> signInWithGoogle();
+
+  Future<Either<ServerException, UserEntity>> signInWithFacebook();
+
+  Future<Either<ServerException, void>> verifyAccount();
+
+  Future<Either<ServerException, void>> signOut();
+}
