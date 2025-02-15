89c66e23d4ec2863fcc42061a6037bcdb6e03d5d
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-02-16 01:23:44 +0200

    Created forget password states

---
+++ lib/features/auth/view/view_models/forget_password_view_model/forget_password_states.dart
@@ -0,0 +1,21 @@
+sealed class ForgetPasswordStates {}
+
+class ForgetPasswordInitialState extends ForgetPasswordStates {}
+
+class ForgetPasswordLoadingState extends ForgetPasswordStates {}
+
+class ForgetPasswordHideLoadingState extends ForgetPasswordStates {}
+
+class ForgetPasswordSuccessState extends ForgetPasswordStates {
+  final String message;
+
+  ForgetPasswordSuccessState(this.message);
+}
+
+class ForgetPasswordErrorState extends ForgetPasswordStates {
+  final String message;
+
+  ForgetPasswordErrorState(this.message);
+}
+
+class NavigateToLoginScreenState extends ForgetPasswordStates {}
