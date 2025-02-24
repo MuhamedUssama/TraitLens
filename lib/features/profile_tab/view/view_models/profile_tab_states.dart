1c2644cf8a6517e21a592dde5a3b5768d172f8cf
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-02-24 07:06:18 +0200

    Created profile tab states

---
+++ lib/features/profile_tab/view/view_models/profile_tab_states.dart
@@ -0,0 +1,9 @@
+sealed class ProfileTabStates {}
+
+class ProfileTabInitialState extends ProfileTabStates {}
+
+class ProfileTabLoadingState extends ProfileTabStates {}
+
+class GetUserDataSuccessState extends ProfileTabStates {}
+
+class GetUserDataErrorState extends ProfileTabStates {}
