7f28f0aef43e980053f20f3d1f7d920edc4a3c04
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-03-01 23:32:17 +0200

    Make background has a background image to make it more pretty ^^

--- lib/features/profile_tab/view/ui/edit_profile_screen.dart
+++ lib/features/profile_tab/view/ui/edit_profile_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../config/theme/text_style.dart';
 import '../../../../core/di/di.dart';
+import '../../../../core/utils/app_assets.dart';
 import '../../../../core/utils/app_dialogs.dart';
 import '../../../fill_profile/domain/entities/fill_profile_entity.dart';
 import '../view_model/edit_profile_screen/edit_profile_screen_states.dart';
@@ -53,53 +54,59 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   Widget build(BuildContext context) {
     AppLocalizations? locale = AppLocalizations.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          locale!.editProfile,
-          style: TextStyles.font24BlackMedium,
-        ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(image: AssetImage(AppAssets.background)),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 22.w),
-          child:
-              BlocListener<EditProfileScreenViewModel, EditProfileScreenStates>(
-            bloc: viewModel,
-            listener: (context, state) {
-              if (state is EditProfileFailureState) {
-                AppDialogs.showFailDialog(
-                  message: state.message ?? locale.somethingWentWrong,
-                  context: context,
-                  posActionTitle: locale.ok,
-                );
-              }
-              if (state is EditProfileSuccessState) {
-                AppDialogs.showSuccessDialog(
-                  message: locale.fillProfileSuccessfully,
-                  context: context,
-                  posActionTitle: locale.ok,
-                );
-              }
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_new),
+            onPressed: () {
+              Navigator.pop(context);
             },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 50.h),
-                UserImageWidget(
-                  user: arguments!,
-                  viewModel: viewModel,
-                ),
-                SizedBox(height: 50.h),
-                EditScreenFormData(viewModel: viewModel, user: arguments!),
-              ],
+          ),
+          title: Text(
+            locale!.editProfile,
+            style: TextStyles.font24BlackMedium,
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 22.w),
+            child: BlocListener<EditProfileScreenViewModel,
+                EditProfileScreenStates>(
+              bloc: viewModel,
+              listener: (context, state) {
+                if (state is EditProfileFailureState) {
+                  AppDialogs.showFailDialog(
+                    message: state.message ?? locale.somethingWentWrong,
+                    context: context,
+                    posActionTitle: locale.ok,
+                  );
+                }
+                if (state is EditProfileSuccessState) {
+                  AppDialogs.showSuccessDialog(
+                    message: locale.fillProfileSuccessfully,
+                    context: context,
+                    posActionTitle: locale.ok,
+                  );
+                }
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50.h),
+                  UserImageWidget(
+                    user: arguments!,
+                    viewModel: viewModel,
+                  ),
+                  SizedBox(height: 50.h),
+                  EditScreenFormData(viewModel: viewModel, user: arguments!),
+                ],
+              ),
             ),
           ),
         ),
