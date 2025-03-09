1dfaa4ab152515f05599feb25c7f08eb00860483
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-03-09 07:33:15 +0200

    Created text detection screen states

---
+++ lib/features/ai_detection/view/view_models/text_view_model/text_detection_states.dart
@@ -0,0 +1,19 @@
+import '../../../data/models/text/text_detection_result_model.dart';
+
+sealed class TextDetectionStates {}
+
+class TextDetectionInitialState extends TextDetectionStates {}
+
+class TextDetectionLoadingState extends TextDetectionStates {}
+
+class TextDetectionFailureState extends TextDetectionStates {
+  final String? exception;
+  TextDetectionFailureState(this.exception);
+}
+
+class TextDetectionSuccessState extends TextDetectionStates {
+  final TextDetectionResultModel? textModel;
+  TextDetectionSuccessState(this.textModel);
+}
+
+class NavigateToAudioScreenState extends TextDetectionStates {}
