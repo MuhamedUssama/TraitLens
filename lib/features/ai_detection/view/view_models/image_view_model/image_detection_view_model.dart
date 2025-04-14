de191bae61e7dea866a698935df4ceba8a572544
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-14 06:19:07 +0200

    Added face detection feature to validate image if it has a person face or something else

--- lib/features/ai_detection/view/view_models/image_view_model/image_detection_view_model.dart
+++ lib/features/ai_detection/view/view_models/image_view_model/image_detection_view_model.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
 import 'package:injectable/injectable.dart';
 import 'package:trait_lens/core/utils/image_picker_utils.dart';
 import 'package:trait_lens/features/ai_detection/domain/usecases/send_image_usecase.dart';
@@ -19,6 +21,13 @@ class ImageDetectionViewModel extends Cubit<ImageDetectionStates> {
     if (imageFile != null) {
       emit(ImageDetectionLoadingState());
 
+      bool hasFace = await detectFaceInImage(imageFile!);
+
+      if (!hasFace) {
+        emit(ImageDetectionErrorState("No face detected in the image."));
+        return;
+      }
+
       final result = await _usecase(imageFile: imageFile!);
 
       result.fold(
@@ -28,6 +37,27 @@ class ImageDetectionViewModel extends Cubit<ImageDetectionStates> {
       );
     } else {
       emit(ImageDetectionErrorState("Please select an image first."));
+    }
+  }
+
+  Future<bool> detectFaceInImage(File imageFile) async {
+    final InputImage inputImage = InputImage.fromFile(imageFile);
+
+    FaceDetectorOptions options = FaceDetectorOptions(
+      performanceMode: FaceDetectorMode.accurate,
+      enableContours: false,
+      enableLandmarks: false,
+    );
+
+    FaceDetector faceDetector = FaceDetector(options: options);
+
+    try {
+      List<Face> faces = await faceDetector.processImage(inputImage);
+      return faces.isNotEmpty;
+    } catch (e) {
+      return false;
+    } finally {
+      await faceDetector.close();
     }
   }
 
