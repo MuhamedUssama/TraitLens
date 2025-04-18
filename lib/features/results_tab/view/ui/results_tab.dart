625f95b2cb437e413727bdec7c032944e8f580ed
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-18 02:18:03 +0200

    Created loading card widget in result tab

--- lib/features/results_tab/view/ui/results_tab.dart
+++ lib/features/results_tab/view/ui/results_tab.dart
@@ -1,13 +1,16 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:trait_lens/config/theme/app_colors.dart';
 import 'package:trait_lens/core/di/di.dart';
 import 'package:trait_lens/core/utils/animation_assets.dart';
 import 'package:trait_lens/features/results_tab/view/view_models/results_tab_states.dart';
 import 'package:trait_lens/features/results_tab/view/view_models/results_tab_view_model.dart';
 import 'package:trait_lens/features/results_tab/view/widgets/custom_result_card.dart';
+import 'package:trait_lens/features/results_tab/view/widgets/cutom_result_card_skeleton.dart';
 
 class ResultsTab extends StatelessWidget {
   ResultsTab({super.key});
@@ -22,9 +25,20 @@ class ResultsTab extends StatelessWidget {
           bloc: viewModel,
           builder: (context, state) {
             if (state is ResultTabsLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: ColorsManager.baseBlue,
+              return Expanded(
+                child: Skeletonizer(
+                  enabled: true,
+                  child: ListView.separated(
+                    itemCount: 3,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemBuilder: (context, index) {
+                      return const CustomResultCardSkeleton();
+                    },
+                  ),
                 ),
               );
             } else if (state is ResultTabsErrorState) {
@@ -36,7 +50,10 @@ class ResultsTab extends StatelessWidget {
               );
             } else if (state is ResultTabsEmptyListState) {
               return Expanded(
-                  child: LottieBuilder.asset(AnimationsAssets.emptyList));
+                child: FadeIn(
+                  child: LottieBuilder.asset(AnimationsAssets.emptyList),
+                ),
+              );
             } else if (state is ResultTabsLoadedState) {
               return Expanded(
                 child: ListView.separated(
@@ -47,9 +64,12 @@ class ResultsTab extends StatelessWidget {
                     return SizedBox(height: 16.h);
                   },
                   itemBuilder: (context, index) {
-                    return CustomResultCard(
-                      viewModel: viewModel,
-                      detectionResul: state.results[index],
+                    return FadeInLeft(
+                      delay: Duration(milliseconds: 100 * index),
+                      child: CustomResultCard(
+                        viewModel: viewModel,
+                        detectionResul: state.results[index],
+                      ),
                     );
                   },
                 ),
